e00bdb671d237c4412e13e821c7138903c60ae0a
Froshle14 <kimkilwan135@gmail.com>
2023-12-10 22:41:34 -0600

    Tabla de productos

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,23 +4,52 @@
 
 @section('content')
     <div class="container">
-        <h1>Lista de Productos</h1>
-        <a href="{{ route('products.create') }}" class="btn btn-primary">Agregar Nuevo Producto</a>
-        <ul class="list-group mt-3">
+        <div>
+            <h1>Lista de Productos</h1>
+             <a href="{{ route('products.create') }}" class="btn btn-primary">Agregar Nuevo Producto</a> <!-- Esto debe ir a la derecha -->
+        </div>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Categoria</th>
+                <th>Precio actual</th>
+                <th>Stock</th>
+                <th>Distribuidor</th>
+            </tr>
             @foreach ($products as $product)
-                <li class="list-group-item">
-                    {{ $product->name }} - {{ $product->category->name }} - {{ $product->supplier->name }}
-                    <div class="float-right">
+                <tr>
+                    <td>
+                        {{ $product->name }}
+                    </td>
+                    <td>
+                        {{ $product->category->name }}
+                    </td>
+                    <td>
+                        {{ $product->current_price }}
+                    </td>
+                    <td>
+                        {{ $product->stock }}
+                    </td>
+                    <td>
+                        {{ $product->supplier->name }}
+                    </td>
+                    <!-- <td>
                         <a href="{{ route('products.show', $product->product_id) }}" class="btn btn-info btn-sm">Ver</a>
+                    </td> -->
+                    <td>
                         <a href="{{ route('products.edit', $product->product_id) }}" class="btn btn-warning btn-sm">Editar</a>
+                    </td>
+                    <td>
                         <form action="{{ route('products.destroy', $product->product_id) }}" method="POST" class="d-inline">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('¿Estás seguro?')">Eliminar</button>
                         </form>
-                    </div>
-                </li>
+                    </td>
+                        
+                        
+                </tr>
             @endforeach
-        </ul>
+        </table>
     </div>
 @endsection
