9c3183325d119eca3d3ecf8187a76373b075dd07
PhantomDio <39938060+PhantomDio@users.noreply.github.com>
2023-12-10 20:39:12 -0600

    Formato lista Suppliers

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -3,20 +3,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Suppliers List</h1>
-    <a href="{{ route('suppliers.create') }}">Add New Supplier</a>
-    <ul>
-        @foreach ($suppliers as $supplier)
-            <li>
-                {{ $supplier->name }}
-                <a href="{{ route('suppliers.show', $supplier->supplier_id) }}">View</a>
-                <a href="{{ route('suppliers.edit', $supplier->supplier_id) }}">Edit</a>
-                <form action="{{ route('suppliers.destroy', $supplier->supplier_id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <h1>Lista de Proveedores</h1>
+        <a href="{{ route('suppliers.create') }}" class="btn btn-primary">Agregar Nuevo Proveedor</a>
+        <ul class="list-group mt-3">
+            @foreach ($suppliers as $supplier)
+                <li class="list-group-item">
+                    {{ $supplier->name }}
+                    <div class="float-right">
+                        <a href="{{ route('suppliers.show', $supplier->supplier_id) }}" class="btn btn-info btn-sm">Ver</a>
+                        <a href="{{ route('suppliers.edit', $supplier->supplier_id) }}" class="btn btn-warning btn-sm">Editar</a>
+                        <form action="{{ route('suppliers.destroy', $supplier->supplier_id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('¿Estás seguro?')">Eliminar</button>
+                        </form>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
