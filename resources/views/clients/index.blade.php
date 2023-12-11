bf9591c5e2acfab50753dbb8aa5a90be2038eb88
Froshle14 <kimkilwan135@gmail.com>
2023-12-10 21:29:31 -0600

    Tabla clientes

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -6,21 +6,34 @@
     <div class="container">
         <h1>Lista de Clientes</h1>
         <a href="{{ route('clients.create') }}" class="btn btn-primary">Agregar Nuevo Cliente</a>
-        <ul class="list-group mt-3">
+        <table>
+            <tr>
+                <th>RUT</th>
+                <th>Nombre</th>
+            </tr>
             @foreach ($clients as $client)
-                <li class="list-group-item">
-                    {{ $client->name }}
+                <tr>
+                    
                     <div class="float-right">
-                        <a href="{{ route('clients.show', $client->client_id) }}" class="btn btn-info btn-sm">Ver</a>
-                        <a href="{{ route('clients.edit', $client->client_id) }}" class="btn btn-warning btn-sm">Editar</a>
-                        <form action="{{ route('clients.destroy', $client->client_id) }}" method="POST" class="d-inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('¿Estás seguro?')">Eliminar</button>
-                        </form>
+                        <td>
+                            {{ $client->client_id }}
+                        </td>
+                        <td>
+                            {{ $client->name }}
+                        </td>
+                        <td>
+                            <a href="{{ route('clients.edit', $client->client_id) }}" class="btn btn-warning btn-sm">Editar</a>
+                        </td>
+                        <td>
+                            <form action="{{ route('clients.destroy', $client->client_id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('¿Estás seguro?')">Eliminar</button>
+                            </form>
+                        </td>
                     </div>
-                </li>
+                </tr>
             @endforeach
-        </ul>
+</table>
     </div>
 @endsection
